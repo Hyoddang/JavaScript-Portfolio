d34e1b4bc4f89950637ad7a6772511631ac359ff
Hyoddang <yhd5689@naver.com>
2024-11-01 20:09:50 +0900

    페이지네이션 스타일링, 기능 구현틀 작성

--- 게시판/JS/pagination.js
+++ 게시판/JS/pagination.js
@@ -1,1 +1,13 @@
-const 
+function pagination() {
+  const paginationNum = document.querySelector(".pagination-wrap");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const paginationBtnWrap = document.querySelector(".pagination-num-wrap")
+  
+  const COUNT_DISPLAY_PAGE = 9; // 한 페이지당 최대 9개의 게시물을 보여줌
+  const TOTAL_COUNT = notifys.length; // 게시물 총합
+  
+  let totalPage = Math.ceil(TOTAL_COUNT / COUNT_DISPLAY_PAGE); // 페이지네이션 길이
+  
+  
+}
